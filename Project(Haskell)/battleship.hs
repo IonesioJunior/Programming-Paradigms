6c7505eee44d7eb59a4050333cb297b4a23c6f52
Ionesio Junior <ionesiojr@gmail.com>
2017-07-11 22:17:35 -0300

    Added game loop, simple mapMat and other functions

--- Project(Haskell)/battleship.hs
+++ Project(Haskell)/battleship.hs
@@ -12,21 +12,33 @@ authors : Ionésio Junior
 
 import System.IO.Unsafe
 import System.Random
-
-buildMat :: Int -> Int -> [[Int]]
-buildMat n m = [     [0 | x <- [1,2 .. n] ]     |   y <- [1,2 .. m]       ]
+import Control.Monad
 
 
+-- Build Matrix with value and position of the hiden ships
+buildMat :: Int -> Int -> [[(Int,Int)]] -> [[Int]]
+buildMat n m coords = [     [ fillMat (x,y) | x <- [1,2 .. n] ]     |   y <- [1,2 .. m]       ]
+
+--show matrix to player
 showMat :: [[Int]] -> String
-showMat m  = concat (concat [ [ "~ " | y <- x] ++ ["\n"] | x <- m])
+showMat m  = concat (concat [ [ mapMat y | y <- x] ++ ["\n"] | x <- m])
 
+mapMat :: Int -> String
+mapMat value | (value == 0) = "~ "
+	     | (value == 1) = "1 "
+	     | (value == 2) = "2 "
+	     | (value == 3) = "3 "
+	     | (value == -1) = "X "
+
+--Generate random coords for first position of ships
 generateRandomCoords :: Int -> Int -> [(Int,Int)]
 generateRandomCoords a b =[ (unsafePerformIO (getStdRandom (randomR (a, b))),unsafePerformIO (getStdRandom (randomR (a, b)))) | y <- [1,2 .. 7]]
 
+--Generate coord for cruiser using random seed
 cruiserCoords :: [(Int,Int)] -> [[(Int,Int)]]
 cruiserCoords a = [ [a !! 0 , a !! 1] , [a !! 2, a !! 3] ]
 
-
+--Generate coord for battleship  using random seed
 generateBattleShipsCoords :: [(Int,Int)] -> [[(Int,Int)]]
 generateBattleShipsCoords randomCoord = do
 					  let battleship = [ (x + i,y) | (x,y) <- [head randomCoord],i <- [0,1 .. 3]]
@@ -34,9 +46,11 @@ generateBattleShipsCoords randomCoord = do
 					  [battleship] ++ cruiser ++ [ [(x,y)] | (x,y) <- [randomCoord !! i | i <- [3,4 .. 6]]]
 
 
+-- static value
 coords = generateBattleShipsCoords (generateRandomCoords 0 5)
 
 
+--Show simple legends
 displayMenu :: IO()
 displayMenu = putStrLn("======= Batalha Naval ========\n"++
   "O - Desconhecido\n"++
@@ -46,6 +60,27 @@ displayMenu = putStrLn("======= Batalha Naval ========\n"++
 	"3 - Minesweeper\n\n"++
 	"Quantos disparos deseja ter?\n")
 
+--Game loop
+loop :: [[Int]] -> Int -> Int -> Int -> IO()
+loop matrix shots fired hit | (shots == fired)  = putStrLn $ (getResult hit)
+		            | otherwise = do 
+						putStrLn(" ========  BattleShip  ======== \n\n")
+						putStrLn $ showMat $ matrix
+						putStrLn(" Onde deseja atirar? \n")
+						shot <- getLine
+						loop matrix shots (fired + 1) hit
+						
+					
+
+--Define if you win or loose
+getResult :: Int -> String
+getResult hit | (hit == 12) = "You Win!\n"
+	      | otherwise = "You Lose!\n"
+
+
 main = do
-		displayMenu
-		print $ coords
+	   displayMenu
+	   input <- getLine
+	   let shots = (map read $ words input :: [Int])
+	   let matrix = buildMat 9 9 coords
+	   loop matrix (shots !! 0) 0 0

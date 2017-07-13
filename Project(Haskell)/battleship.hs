9c1440c969cb05d9f65dc82e31830fd4aeb76e6e
wesleyanibal <wesley.anibal@ccc.ufcg.edu.br>
2017-07-13 14:31:47 -0300

    ajustes contains e fillMat

--- Project(Haskell)/battleship.hs
+++ Project(Haskell)/battleship.hs
@@ -17,18 +17,42 @@ import Control.Monad
 
 -- Build Matrix with value and position of the hiden ships
 buildMat :: Int -> Int -> [[(Int,Int)]] -> [[Int]]
-buildMat n m coords = [     [ fillMat (x,y) | x <- [1,2 .. n] ]     |   y <- [1,2 .. m]       ]
+buildMat n m coords = [     [ 0 | x <- [1,2 .. n] ]     |   y <- [1,2 .. m]       ]
+
+containsBattleShip :: (Int, Int) -> [(Int, Int)] -> Bool
+containsBattleShip tupla coords1 | (coords1 !! 0) == tupla = True
+                                | (coords1 !! 1) == tupla = True
+                                | (coords1 !! 2) == tupla = True
+                                | (coords1 !! 3) == tupla = True
+                                | otherwise = False
+
+fillMat :: Int -> Int -> [(Int, Int)] -> Int
+fillMat x y coords | containsBattleShip (x,y) (coords !! 0) = 1
+                   | containsCruiser (x,y) (coords !! 1) (coords !! 2) = 2
+                   | containsMinesweeper (x,y) (coords !! 3) (coords !! 4) (coords !! 5) (coords !! 6) = 3
+                   | otherwise = 5
+
+
+containsCruiser :: (Int,Int) -> [(Int,Int)] -> [(Int, Int)] -> Bool
+containsCruiser tupla coords1 coords2 | (coords1 !! 0) == tupla || (coords2 !! 0 ) == tupla = True
+                                      | (coords1 !! 1) == tupla || (coords2 !! 1 ) == tupla = True
+                                      | otherwise = False
+
+containsMinesweeper :: (Int,Int) -> [(Int,Int)] -> [(Int, Int)] -> [(Int, Int)] -> [(Int, Int)] -> Bool
+containsMinesweeper tupla coords1 coords2 coords3 coords4 | (coords1 !! 0) == tupla || (coords2 !! 0 ) == tupla || (coords3 !! 0) == tupla || (coords4 !! 0 ) == tupla = True
+                                                          | otherwise = False
+
 
 --show matrix to player
 showMat :: [[Int]] -> String
 showMat m  = concat (concat [ [ mapMat y | y <- x] ++ ["\n"] | x <- m])
 
 mapMat :: Int -> String
-mapMat value | (value == 0) = "~ "
-	     | (value == 1) = "1 "
-	     | (value == 2) = "2 "
-	     | (value == 3) = "3 "
-	     | (value == -1) = "X "
+mapMat value | (value == 1) = "1 "
+	           | (value == 2) = "2 "
+	           | (value == 3) = "3 "
+	           | (value == 5) = "X "
+             | otherwise = "~ "
 
 --Generate random coords for first position of ships
 generateRandomCoords :: Int -> Int -> [(Int,Int)]
@@ -63,14 +87,14 @@ displayMenu = putStrLn("======= Batalha Naval ========\n"++
 --Game loop
 loop :: [[Int]] -> Int -> Int -> Int -> IO()
 loop matrix shots fired hit | (shots == fired)  = putStrLn $ (getResult hit)
-		            | otherwise = do 
-						putStrLn(" ========  BattleShip  ======== \n\n")
-						putStrLn $ showMat $ matrix
-						putStrLn(" Onde deseja atirar? \n")
-						shot <- getLine
-						loop matrix shots (fired + 1) hit
-						
-					
+		                        | otherwise = do
+						                                  putStrLn(" ========  BattleShip  ======== \n\n")
+						                                  putStrLn $ showMat $ matrix
+						                                  putStrLn(" Onde deseja atirar? \n")
+						                                  shot <- getLine
+						                                  loop matrix shots (fired + 1) hit
+
+
 
 --Define if you win or loose
 getResult :: Int -> String

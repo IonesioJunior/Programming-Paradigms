05f3d62d521de5be0eaedd785ee18d7d3ad53e22
AgnaldoCC <agnaldo.junior@ccc.ufcg.edu.br>
2017-08-17 19:53:28 -0300

    Main Criado; Numero de tiros agora escolhido pelo usuário

--- Project(Prolog)/battleship.pl
+++ Project(Prolog)/battleship.pl
@@ -16,6 +16,28 @@ inserirNavios(Tabuleiro, NovoTabuleiro):-
 	insertMinesweeper(Tabuleiro6, Tabuleiro7),
 	insertMinesweeper(Tabuleiro7, NovoTabuleiro).
 
+/* Execução da lógica sequencial do jogo */
+
+play(Tabuleiro, Misseis) :-
+  Misseis > 0,
+  imprimeTabuleiroJogador(Tabuleiro),
+  atirar(Tabuleiro, NovoTabuleiro), NovosMisseis is Misseis-1,
+  (
+  not( existemNavios(NovoTabuleiro) ) -> imprimeTabuleiroReal(NovoTabuleiro), vitoria;
+ (NovosMisseis > 1 -> misseis(NovosMisseis), jogar(NovoTabuleiro, NovosMisseis);
+  NovosMisseis =:= 1 -> ultimoMissel, jogar(NovoTabuleiro, NovosMisseis);
+  NovosMisseis =:= 0 -> misseisEsgotados, imprimeTabuleiroReal(NovoTabuleiro), gameOver)
+  ).
+
+/* Execução do programa */
+:- initialization(main).
+main :-
+   write("Digite a quantidade de tiros que deseja ter: "),
+   read(N),
+   gerarTabuleiro(TabuleiroRamdomicoAux),
+   inserirNavios(TabuleiroRamdomicoAux, TabuleiroRamdomico),
+   play(TabuleiroRamdomico, N).
+
 
 
 
